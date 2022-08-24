e959f4200bb8d33445a8f7412affd3149ffae2a2
AlisonOsnir <93959397+AlisonOsnir@users.noreply.github.com>
2022-08-24 17:27:26 -0300

    Begin writeCSV

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Tesseract from 'tesseract.js';
 import chalk from 'chalk';
 import Jimp from 'jimp';
+import * as fs from 'fs';
 
 let img = ('./images/img1.png'); // Ler varias fotos.
 let part0;
@@ -8,16 +9,17 @@ let part1;
 let part2;
 let recognizedText;
 let formatedText;
+let arrValues = [];
 
 function main(image) {
     cropImage(image);
-    setTimeout(() => ocrImage(0), 1 * 1000)
-    setTimeout(() => ocrImage(1), 8 * 1000)
-    setTimeout(() => ocrImage(2), 22 * 1000)
-    setTimeout(() => mergeText(part0, part1, part2), 35 * 1000)
-    setTimeout(() => formatText(recognizedText), 36 * 1000)
-    setTimeout(() => findValues(formatedText), 37 * 1000)
-    //setTimeout(() => writeCSV(), 38 * 1000) // Proxima passo.
+    setTimeout(() => ocrImage(0), 1 * 1000);
+    setTimeout(() => ocrImage(1), 8 * 1000);
+    setTimeout(() => ocrImage(2), 22 * 1000);
+    setTimeout(() => mergeText(part0, part1, part2), 35 * 1000);
+    setTimeout(() => formatText(recognizedText), 36 * 1000);
+    setTimeout(() => findValues(formatedText), 37 * 1000);
+    //setTimeout(() => writeCSV(arrValues), 38 * 1000); // Proxima passo.
 }
 
 main(img)
@@ -55,7 +57,7 @@ function ocrImage(part) {
 
 function mergeText(a, b, c) {
     recognizedText = (a + b + c);
-    console.log(recognizedText) // Show the concatened text. 
+    //console.log(recognizedText) // Show the concatened text. 
 }
 
 function formatText(data){
@@ -82,29 +84,53 @@ const regExp = [
 function findValues(text) {
     let repeatCalibraçãoSaida = true;
     let repeatCalibraçãoTC = true;
-    
+
     for (let i = 0; i < regExp.length; i++) {
         let result = regExp[i].exec(text); 
         if (result) {
             result = result.toString();
-            if (i == 0) { console.log(result.slice(-6)) }
-            if (i == 1) { console.log(result.slice(-5)) }
-            if (i == 2) { console.log(result.slice(-5)) }
-            if (i == 3) { console.log(result.slice(-4)) }
-            if (i == 4) { console.log(result.slice(-4)) }
-            if (i == 5) { console.log(result.slice(-2)) }
-            if (i == 6) { console.log(result.slice(-2)) }
-            if (i == 7) { console.log(result.slice(-2)) }
-            if (i == 8) { console.log(result.slice(-2)) }
+            if (i == 0) { arrValues.push(result.slice(-6)) }
+            if (i == 1) { arrValues.push(result.slice(-5)) }
+            if (i == 2) { arrValues.push(result.slice(-5)) }
+            if (i == 3) { arrValues.push(result.slice(-4)) }
+            if (i == 4) { arrValues.push(result.slice(-4)) }
+            if (i == 5) { arrValues.push(result.slice(-2)) }
+            if (i == 6) { arrValues.push(result.slice(-2)) }
+            if (i == 7) { arrValues.push(result.slice(-2)) }
+            if (i == 8) { arrValues.push(result.slice(-2)) }
             
             if (i == 4 && repeatCalibraçãoTC) { i = 2; repeatCalibraçãoTC = false; }
             if (i == 8 && repeatCalibraçãoSaida) { i = 4; repeatCalibraçãoSaida = false; }
 
             text = text.replace(result, (chalk.bgGreen('#')))
         } else if (result == undefined) {                        
-            console.log(chalk.red('RegExp not found'))
+            arrValues.push('RegExp not found')
         }
     }
+    console.log(arrValues)
     //console.log(text); //show where the date was obtained.
 }
 
+
+let writeStream = fs.createWriteStream('./values.csv')
+function writeCSV(someArrayOfObjects){
+
+    someArrayOfObjects.forEach((someObject, index) => {     
+        let newLine = []
+        newLine.push(someObject.stringPropertyOne)
+        newLine.push(someObject.stringPropertyTwo)
+        
+    
+        writeStream.write(newLine.join(',')+ '\n', () => {
+            // a line was written to stream
+        })
+    })
+    
+    writeStream.end()
+    
+    writeStream.on('finish', () => {
+        console.log('finish write stream, moving along')
+    }).on('error', (err) => {
+        console.log(err)
+    })
+}
