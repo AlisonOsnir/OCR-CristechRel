5f265a6c0d147aa3caf49644e11bde5448020e5d
AlisonOsnir <alison_osnir@hotmail.com>
2022-09-01 00:03:01 -0300

    Refactoring / Error Handling

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ const cropCoords = {
   'part1': [1, 20, 200, 30],
   'part2': [278, 100, 237, 460],
   'part3': [510, 100, 237, 460]
-}
+};
 
 const headerTM = [
   'Serial', 'RTD-A', 'RTD-B', 'TC1-CR', 'TC1-CL', 'TC2-CR', 'TC2-CL',
   'Saida_mA1-1mA', 'Saida_mA1-5mA', 'Saida_mA1-10mA', 'Saida_mA1-20mA',
   'Saida_mA2-1mA', 'Saida_mA2-5mA', 'Saida_mA2-10mA', 'Saida_mA2-20mA'
-]
+];
 
 const regExpTM = {
   'Serial': /númerodesérie:\d{6}/i,
@@ -28,61 +28,68 @@ const regExpTM = {
   'Saida-5mA': /[s5]ma:?\d{2}/i,
   'Saida-10mA': /10ma:?\d{2}/i,
   'Saida-20mA': /20ma:?\d{2}/i,
-}
+};
 
-async function cropImage(img) {
-  console.log(chalk.blue(`>>> ImagePath: ${img}`));
-  for (let i = 1; i < 4; i++) {
-    const image = await Jimp.read(img);
-    image.grayscale()
-    image.crop(...cropCoords[Object.keys(cropCoords)[i - 1]]);
-    await image.writeAsync(`cropped_images/cropped_part${i}.png`);
+function writeHeader(header) {
+  fs.writeFile(txtFilePath, header.join(" ") + '\n', 'utf8', (err) => {
+    if (err) throw new Error('Failed to write header', err);
+  })
+  console.log(chalk.green('>>> Header writed to file.\n'));
+};
+
+async function cropImage(imgPath) {
+  console.log(chalk.blue(`>>> ImagePath: ${imgPath}`));
+  try {
+    for (let i = 0; i < 3; i++) {
+      const image = await Jimp.read(imgPath);
+      image.grayscale();
+      image.crop(...cropCoords[Object.keys(cropCoords)[i]]);
+      await image.writeAsync(`cropped_images/cropped_part${i + 1}.png`);
+    }
+  } catch (error) {
+    throw new Error('Failed to crop the image: ', error)
   }
-  console.log('>>> cropImage Done.')
-}
+  console.log('>>> cropImage Done.');
+};
 
 async function ocrImage(part) {
   console.log(`>>> OCR cropped_${part} Start`);
-
   try {
     const worker = createWorker({
       //logger: m => console.log(m), // Add logger here
     });
-
     await worker.load();
     await worker.loadLanguage('por');
     await worker.initialize('por');
     const { data: { text } } = await worker.recognize(`./cropped_images/cropped_${part}.png`);
-    //console.log(text);
     await worker.terminate();
-    return new Promise((resolve, reject) => {
-      resolve(text)
-    })
+    return text;
+
   } catch (error) {
-    console.log("error" + error);
+    throw new Error('Failed to recognize characters: ', error);
   } finally {
     console.log(`>>> OCR cropped_${part} Done.`);
   }
   sleep(6000);
-}
+};
 
-function processData(a, b, c) {
-  let text = (a + b + c);
-  if (!text) throw TypeError;
-  text = text.replace(/\s/g, '');
-  text = text.replace("'", '');
-  text = text.replace('"', '');
-  console.log('>>> Data process Done.');
-  return text;
-
-}
+function processData(text1, text2, text3) {
+  let data = (text1 + text2 + text3);
+  if (!data) throw new Error('ProcessData must receive string type');              // REFATORAR Se typeof text1, text2 ou text3... não for string
+  data = data.replace(/\s/g, '');
+  data = data.replace("'", '');
+  data = data.replace('"', '');
+  console.log('>>> processData Done.');
+  return data;
+};
 
 function getValues(text) {
   let repeatCalibraçãoSaida = true;
   let repeatCalibraçãoTC = true;
   const arr = [];
+  let acc = 0;
 
-  for (let i = 0; i < Object.keys(regExpTM).length; i++) {
+  for (let i = 0; i < Object.keys(regExpTM).length; i++) {       //REFATORAR TIPO DE LOOP
     let result = Object.values(regExpTM)[i].exec(text);
     if (result) {
       result = result.toString();
@@ -101,62 +108,56 @@ function getValues(text) {
 
       text = text.replace(result, (chalk.bgGreen('###')));
     } else if (result == undefined) {
-      arr.push('N/A');
-      //console.log(chalk.red('regExp not found'));
+      acc++;
+      arr.push('N/A');     //arr.push('#ERROR#')      //Deveria aceitar n/a ou o valor (regexp)
     }
   }
-  //console.log(arr);
-  //console.log(text);
-  console.log('>>> getValues Done.')
-  return arr
-}
+  console.error(chalk.red(`>>> ${acc} regExp not found.`));
+  console.log('>>> getValues Done.');
+  return arr;
+};
 
 async function appendValues(arrValues) {
-  fs.appendFile(txtFilePath, arrValues.join(" ") + '\n', 'utf8', function (err) {
-    if (err) throw err;
+  fs.appendFile(txtFilePath, arrValues.join(" ") + '\n', 'utf8', (err) => {
+    if (err) throw new Error('Failed to append values: ', err);
   })
 
-  //sleep(6000)
-  console.log(chalk.green('>>> Values Appended to textFile.\n'));
-}
-
-function writeHeader(header) {
-  fs.writeFile(txtFilePath, header.join(" ") + '\n', 'utf8', function (err) {
-    if (err) throw err;
-  })
-  console.log(chalk.green('>>> Header writed to textFile.\n'));
-}
-
-function counterImagesLog(index, files) {
-  console.log(chalk.green(`>>> ImagesCounter: ${index + 1}/${files.length}`));
-}
+  sleep(6000);
+  console.log(chalk.green('>>> Values Appended to file.\n'));
+};
 
 function startingLog() {
   console.log(chalk.bgGreen('>>>     Starting Program     <<<\n'));
-}
+};
 
-async function finishLog() {
-  await sleep(5000);
+let counter = 1;
+function counterImagesLog(files) {
+  console.log(chalk.green(`>>> ImagesCounter: ${counter}/${files.length}`));
+  counter++;
+};
+
+function finishLog() {
+  sleep(5000);
   console.log(chalk.bgGreen('>>>     Program Finished     <<<\n'));
-}
+};
 
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-}
+};
 
 async function main(imagePath) {
   try {
-    await cropImage(imagePath)
-    let crop1 = await ocrImage('part1')
-    let crop2 = await ocrImage('part2')
-    let crop3 = await ocrImage('part3')
-    let text = processData(crop1, crop2, crop3)
-    let values = getValues(text)
-    await appendValues(values)
+    await cropImage(imagePath);
+    let crop1 = await ocrImage('part1');
+    let crop2 = await ocrImage('part2');
+    let crop3 = await ocrImage('part3');
+    let text = processData(crop1, crop2, crop3);
+    let values = getValues(text);
+    await appendValues(values);
   } catch (error) {
-    console.log('error:', error)
+    throw new Error('Error running main:', error);
   }
 }
 
@@ -165,19 +166,17 @@ async function main(imagePath) {
     startingLog();
     writeHeader(headerTM);
 
-    // Get the files as an array
     const files = await fs.promises.readdir(imagesDirPath, (err) => {
       if (err) return console.log('Unable to scan directory: ' + err)
     });
 
     // Loop them all with for...of
     for (const file of files) {
-      //counterImagesLog(i, files);
-      await main(imagesDirPath + file)
+      counterImagesLog(files);
+      await main(imagesDirPath + file);
     }
-  }
-  catch (error) {
-    console.error("Error:", error);
+  } catch (error) {
+    console.error('failed to init the program: ', error);
   } finally {
     finishLog();
   }
