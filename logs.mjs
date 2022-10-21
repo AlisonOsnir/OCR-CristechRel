ad921dbe7c818b2f5a78c0f2e1960f46b521b4a9
AlisonOsnir <93959397+AlisonOsnir@users.noreply.github.com>
2022-10-20 21:55:13 -0300

    Add logs module

---
+++ logs.mjs
@@ -0,0 +1,24 @@
+import chalk from 'chalk'
+import path from 'path'
+
+const log = {
+  'start'            : () => console.log(chalk.bgGreen('>>>       Starting Program       <<<\n')),
+  'finish'           : () => console.log(chalk.bgGreen('>>>       Program Finished       <<<')),
+  'addHandFillHeader': () => console.log('>>> addHandFillHeader Done.'),
+  'writeHeader'      : () => console.log(chalk.green('>>> Header writed to file.\n')),
+  'imageName'     : (img) => console.log(chalk.blue(`>>> ImageName: ${path.parse(img).name}`)),
+  'getImageDate' : (date) => console.log(chalk.blue(`>>> ImageDate: ${date}`)),
+  'cropImage'        : () => console.log('>>> cropImage Done.'),
+  'ocrImage'      : (img) => console.log(` OCR cropped ${img} Done.`),
+  'processData'      : () => console.log('>>> processData Done.'),
+  'getValues'        : () => console.log('>>> getValues Done.'),
+  'addHandFillFields': () => console.log('>>> addHandFillFields Done.'),
+  'appendValues'     : () => console.log(chalk.green('>>> Values Appended to file.\n')),
+  'writeExcel'       : () => console.log(chalk.green('>>> writeExcel Done.\n')),
+
+  'getValuesError'   : (acc) => console.error(chalk.red(`>>> Error: ${acc} values not found.`)),
+  'emptyFolderError'    : () => console.error(chalk.red('>>> Screenshot folder is empty!')),
+  'reportValuesError': (key, errors) => console.log(chalk.bgRed(`>>> ${path.parse(key).name} - ${errors[key]} values not found  <<<`))
+}
+
+export default log
